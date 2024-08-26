c473c8e9c8e2562e7cba51953b5d023060fdf43e
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-08-26 20:48:33 +0530

    Create Restaurant.java

---
+++ back-end/src/main/java/com/ABC/back_end/model/Restaurant.java
@@ -0,0 +1,25 @@
+package com.ABC.back_end.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Restaurant {
+
+    //Primary key by @ID (Auto generate)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+}
